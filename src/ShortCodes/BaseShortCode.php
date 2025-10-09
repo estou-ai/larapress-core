adaf39bd6717ef2f14edb2693aea2671e7a17713
Filipe Clemente <51190714+FdClemente@users.noreply.github.com>
2025-10-09 17:32:22 +0100

    Update BaseShortCode.php

--- src/ShortCodes/BaseShortCode.php
+++ src/ShortCodes/BaseShortCode.php
@@ -32,4 +32,55 @@ abstract class BaseShortCode extends BaseElement
     {
         return '';
     }
+
+    protected function renderError($details): false|string
+    {
+        if (current_user_can('administrator')) {
+            return $this->render('errors.php', $details, true);
+        }
+        return "<div></div>";
+    }
+
+    private function generateShortCodeSample($rules): string
+    {
+        $params = $this->generateShortCodeParam($rules);
+
+        $shortcode = $this->generateTag();
+
+        $paramsString = '';
+
+        foreach ($params as $variable => $value) {
+            $paramsString .= $variable.'="'.$value.'" ';
+        }
+
+        return '['.$shortcode.' '.$paramsString.']';
+    }
+
+    protected function validation(array $rules, array $data): false|string
+    {
+        $validator = new Validator();
+        $validation = $validator->validate($data, $rules);
+
+        if ($validation->fails()){
+            $errors = $validation->errors();
+            $shortcode = $this->generateShortCodeSample($rules);
+            return $this->renderError(['errors' => implode(' ', $errors->all('<li>:message</li>')), 'shortcode' => $shortcode]);
+        }
+        return false;
+    }
+
+    private function generateShortcodeParam(array $rules): array
+    {
+        $keys = array_keys($rules);
+
+        $params = [];
+        foreach ($keys as $key) {
+            if (str_contains('required',$rules[$key]))
+                $params[$key] = 'required_param';
+        }
+
+        return $params;
+    }
+
+
 }
