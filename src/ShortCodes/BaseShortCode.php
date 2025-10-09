64299821a4929f5ddf06e78ace877987001c8932
Filipe Clemente <51190714+FdClemente@users.noreply.github.com>
2025-10-09 17:42:15 +0100

    Update BaseShortCode.php

--- src/ShortCodes/BaseShortCode.php
+++ src/ShortCodes/BaseShortCode.php
@@ -3,57 +3,51 @@
 namespace Larapress\ShortCodes;
 
 use Larapress\Dependencies\BaseElement;
+use Rakit\Validation\Validator;
 
 abstract class BaseShortCode extends BaseElement
 {
     protected string $separator = '-';
+
+    public string $shortCodeTag;
+
     public function __construct()
     {
-        $this->generaShortCode();
+        $this->generateShortCode();
     }
 
-    public function generaShortCode()
+    protected function generateShortCode(): void
     {
         $tag = $this->generateTag();
-        add_shortcode($tag,[$this, 'execute']);
+        add_shortcode($tag, [$this, 'execute']);
     }
 
-    public function generateTag()
+    protected function generateTag(): string
     {
-        return $this->slugify(config('app.slug'), $this->getClass());
+        $shortCode = $this->shortCodeTag ?? $this->getClass();
+        return $this->slugify($shortCode);
     }
 
-    public function execute($args):false|string
+    protected function render(string $view, $data, $internalView = false): string
     {
-        $this->render();
-    }
-
-    public function render()
-    {
-        return '';
-    }
-
-    protected function renderError($details): false|string
-    {
-        if (current_user_can('administrator')) {
-            return $this->render('errors.php', $details, true);
+        foreach ($data as $name => $value) {
+            $$name = $value;
         }
-        return "<div></div>";
-    }
+        ob_start();
+        ob_clean();
 
-    private function generateShortCodeSample($rules): string
-    {
-        $params = $this->generateShortCodeParam($rules);
-
-        $shortcode = $this->generateTag();
-
-        $paramsString = '';
-
-        foreach ($params as $variable => $value) {
-            $paramsString .= $variable.'="'.$value.'" ';
+        if ($internalView) {
+            require_once __DIR__ . '/../../resources/Views/errors.php';
+        } else {
+            require_once config('app.views') . $view;
         }
 
-        return '['.$shortcode.' '.$paramsString.']';
+        $html = ob_get_contents();
+        ob_clean();
+        $this->enqueue();
+        //add_action('wp_enqueue_scripts', [$this, 'enqueue']);
+
+        return $html;
     }
 
     protected function validation(array $rules, array $data): false|string
@@ -61,7 +55,7 @@ abstract class BaseShortCode extends BaseElement
         $validator = new Validator();
         $validation = $validator->validate($data, $rules);
 
-        if ($validation->fails()){
+        if ($validation->fails()) {
             $errors = $validation->errors();
             $shortcode = $this->generateShortCodeSample($rules);
             return $this->renderError(['errors' => implode(' ', $errors->all('<li>:message</li>')), 'shortcode' => $shortcode]);
@@ -75,12 +69,44 @@ abstract class BaseShortCode extends BaseElement
 
         $params = [];
         foreach ($keys as $key) {
-            if (str_contains('required',$rules[$key]))
+            if (str_contains('required', $rules[$key]))
                 $params[$key] = 'required_param';
         }
 
         return $params;
     }
 
+    private function generateShortCodeSample($rules): string
+    {
+        $params = $this->generateShortCodeParam($rules);
 
+        $shortcode = $this->generateTag();
+
+        $paramsString = '';
+
+        foreach ($params as $variable => $value) {
+            $paramsString .= $variable . '="' . $value . '" ';
+        }
+
+        return '[' . $shortcode . ' ' . $paramsString . ']';
+    }
+
+    protected function renderError($details): false|string
+    {
+        if (current_user_can('administrator')) {
+            return $this->render('errors.php', $details, true);
+        }
+        return "<div></div>";
+    }
+
+    public function css(): array
+    {
+        return [];
+    }
+
+    public static function getShortcode()
+    {
+        $instance = new static();
+        return $instance->generateTag();
+    }
 }
